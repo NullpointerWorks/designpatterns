426e11866ee57bc7058aede2794f85ada7e72fd2
Nullpointer <38044500+NullpointerWorks@users.noreply.github.com>
2021-01-17 13:21:42 +0100

    Create MainCommand.java

---
+++ src/designpatterns/behavioral/command/MainCommand.java
@@ -0,0 +1,66 @@
+package designpatterns.behavioral.command;
+
+/**
+ * Command Pattern
+ * 
+ */
+public class MainCommand
+{
+	public static void main(String[] args)
+	{
+		
+	}
+}
+
+interface Command
+{
+	void execute();
+}
+
+class SaveCommand implements Command
+{
+	public SaveCommand()
+	{
+		
+	}
+	
+	@Override
+	public void execute() 
+	{
+		
+		
+		
+	}
+}
+
+class LoadCommand implements Command
+{
+	public LoadCommand()
+	{
+		
+	}
+	
+	@Override
+	public void execute() 
+	{
+		
+		
+		
+	}
+}
+
+class CopyCommand implements Command
+{
+	public CopyCommand()
+	{
+		
+	}
+	
+	@Override
+	public void execute() 
+	{
+		
+		
+		
+	}
+}
