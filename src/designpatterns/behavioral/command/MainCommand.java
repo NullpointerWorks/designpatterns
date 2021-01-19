b137b256435a464a019330e94e0148cdbdd903b6
Nullpointer <38044500+NullpointerWorks@users.noreply.github.com>
2021-01-19 15:02:47 +0100

    Update MainCommand.java

--- src/designpatterns/behavioral/command/MainCommand.java
+++ src/designpatterns/behavioral/command/MainCommand.java
@@ -1,7 +1,10 @@
 package designpatterns.behavioral.command;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JButton;
 
 /**
  * Command Pattern
@@ -13,23 +16,54 @@ public class MainCommand
 {
 	public static void main(String[] args)
 	{
+		new MainCommand();
+	}
+	
+	public MainCommand()
+	{
+		JPanel panel = new JPanel();
+		panel.setSize(400,320);
+		panel.setPreferredSize(panel.getSize());
+		
+		Button button = new Button();
+		button.setText("Save command");
+		button.setSize(140,40);
+		button.setPreferredSize(button.getSize());
+		button.setCommand(new SaveCommand());
+		panel.add(button);
+		
 		JFrame window = new JFrame();
+		window.add(panel);
+		window.pack();
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		window.setSize(400,320);
-		window.setPreferredSize(window.getSize());
 		window.setLocationRelativeTo(null);
 		window.setVisible(true);
-		
-		JPanel panel = new JPanel();
-		
-		
-		
-		
-		
-		
-		
-		
-		
+	}
+	
+}
+
+class Button extends JButton implements ActionListener
+{
+	private static final long serialVersionUID = -5245394995966152954L;
+	
+	public Command command;
+	
+	public Button()
+	{
+		super();
+		command = new NullCommand();
+		addActionListener(this);
+	}
+	
+	public void setCommand(Command cmd)
+	{
+		command = cmd;
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent e) 
+	{
+		command.execute();
 	}
 }
 
@@ -39,6 +73,14 @@ public class MainCommand
 interface Command
 {
 	void execute();
+}
+
+class NullCommand implements Command
+{
+	public NullCommand() {}
+	
+	@Override
+	public void execute() {}
 }
 
 class SaveCommand implements Command
@@ -51,34 +93,6 @@ class SaveCommand implements Command
 	@Override
 	public void execute() 
 	{
-		
-	}
-}
-
-class LoadCommand implements Command
-{
-	public LoadCommand()
-	{
-		
-	}
-	
-	@Override
-	public void execute() 
-	{
-		
-	}
-}
-
-class CopyCommand implements Command
-{
-	public CopyCommand()
-	{
-		
-	}
-	
-	@Override
-	public void execute() 
-	{
-		
+		System.out.println("Executing save command..");
 	}
 }
