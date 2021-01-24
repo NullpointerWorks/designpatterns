968f6d28bc240e1d45c4b046ac6689007313524d
Nullpointer <38044500+NullpointerWorks@users.noreply.github.com>
2021-01-24 18:44:56 +0100

    Update MainModelViewAdapter.java

--- src/architecturalpatterns/mva/MainModelViewAdapter.java
+++ src/architecturalpatterns/mva/MainModelViewAdapter.java
@@ -9,7 +9,12 @@ package architecturalpatterns.mva;
  * the back-end. The aim of MVA is to entirely decoupling the Model and View making 
  * sure all interaction is done through the Adapter class. 
  * 
- * 
+ * The example below lets the Adapter have full control over the Model and the View.
+ * As a result, the Adapter tells the View when to do with UI input, and queries the
+ * Model for calculations and results. The connection from the UI to the Adapter is 
+ * done with using the Command pattern with the Standard Library's ActionListener. 
+ * The Adapter sets the command when initializing which keeps the View unaware of 
+ * the controlling object.
  * 
  */
 public class MainModelViewAdapter
