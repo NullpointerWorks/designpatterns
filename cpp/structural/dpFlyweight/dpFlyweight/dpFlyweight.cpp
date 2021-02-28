b4f21e2eab4a4a4e68f2c5d9a11e3131ccd976f9
Nullpointer <38044500+NullpointerWorks@users.noreply.github.com>
2021-02-28 13:04:33 +0100

    update

---
+++ cpp/structural/dpFlyweight/dpFlyweight/dpFlyweight.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <string>
+
+using std::cout;
+using std::endl;
+using std::string;
+
+
+
+class Flywieght
+{
+
+public:
+
+    Flywieght()
+    {
+
+    }
+    ~Flywieght()
+    {
+
+    }
+
+
+};
+
+
+int main()
+{
+
+
+
+
+
+
+
+    std::cout << "Hello World!\n";
+}
